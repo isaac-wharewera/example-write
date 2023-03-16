854f2bb9ef6b13b8bae6f8ae66825779bf5533d6
Isaac  Wharewera <gh9st@Isaacs-MacBook-Pro.local>
2023-03-16 14:32:24 +1300

    working write

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,16 +1,40 @@
 const express = require('express')
-
-const db = require('../db/db')
-
+const fs = require('node:fs/promises')
 const router = express.Router()
+const path = require('node:path')
 
 router.get('/', (req, res) => {
-  db.getUsers()
-    .then((users) => {
-      res.render('index', { users: users })
+  fs.readFile(path.join(__dirname, '../data/name.json'), 'utf-8')
+  res.render('index')
+})
+
+router.post('/name/display', (req, res) => {
+  const nameInput = req.body.name
+  console.log(nameInput)
+  const stringifiedName = JSON.stringify(nameInput)
+  console.log(stringifiedName)
+  fs.writeFile(path.join(__dirname, '../data/name.json'), stringifiedName)
+    .then(() => {
+      res.redirect('/name/display')
     })
     .catch((err) => {
-      res.status(500).send('DATABASE ERROR: ' + err.message)
+      res.status(404).send(err.message)
+    })
+})
+
+router.get('/name/display', (req, res) => {
+  fs.readFile(path.join(__dirname, '../data/name.json'))
+    .then((name) => {
+      const parsedName = JSON.parse(name)
+      const obj = {
+        name: parsedName,
+      }
+
+      res.render('display', obj)
+      // res.render('gamepage', { name })
+    })
+    .catch((err) => {
+      res.status(404).send(err.message)
     })
 })
 
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const hbs = require('express-handlebars')
+const fs = require('node:fs/promises')
 
 const userRoutes = require('./routes/users')
 
@@ -13,5 +14,41 @@ server.use(express.urlencoded({ extended: true }))
 
 // Routes
 server.use('/', userRoutes)
+// testing
+
+// server.get('/', (req, res) => {
+//   fs.readFile(__dirname + '/data/name.json', 'utf-8')
+//   res.render('index')
+// })
+
+// server.post('/name/display', (req, res) => {
+//   const nameInput = req.body.name
+//   console.log(nameInput)
+//   const stringifiedName = JSON.stringify(nameInput)
+//   console.log(stringifiedName)
+//   fs.writeFile(__dirname + '/data/name.json', stringifiedName)
+//     .then(() => {
+//       res.redirect('/name/display')
+//     })
+//     .catch((err) => {
+//       res.status(404).send(err.message)
+//     })
+// })
+
+// server.get('/name/display', (req, res) => {
+//   fs.readFile(__dirname + '/data/name.json')
+//     .then((name) => {
+//       const parsedName = JSON.parse(name)
+//       const obj = {
+//         name: parsedName,
+//       }
+
+//       res.render('display', obj)
+//       // res.render('gamepage', { name })
+//     })
+//     .catch((err) => {
+//       res.status(404).send(err.message)
+//     })
+// })
 
 module.exports = server
